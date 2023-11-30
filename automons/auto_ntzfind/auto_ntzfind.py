f2ab85b5c378eea3cbf21bb332431a84ffadab08
Winston <johnwinstong@gmail.com>
2023-11-30 11:48:04 -0600

    Auto-ntzfind

---
+++ automons/auto_ntzfind/auto_ntzfind.py
@@ -0,0 +1,87 @@
+import sys
+import numpy as np
+from subprocess import PIPE, Popen
+from random import randrange, sample
+
+class Auto_NTZfind:
+
+    def __init__(self, iterations, period):
+        self.golly_file_path =\
+            "/home/winston/devel/play/golly-4.2-src/Scripts/Python/ntzfind_patterns/"
+        self.iterations = iterations
+        self.period = "p" + str(period)
+        self.numbers = list(np.linspace(1,7,7,dtype=int))
+        self.letters =\
+          [
+            ['c', 'e'],
+            ['c', 'e', 'k', 'a', 'i', 'n'],
+            ['c', 'e', 'k', 'a', 'i', 'n', 'y', 'q', 'j', 'r'],
+            ['c', 'e', 'k', 'a', 'i', 'n', 'y', 'q', 'j', 'r', 't', 'w', 'z'],
+            ['c', 'e', 'k', 'a', 'i', 'n', 'y', 'q', 'j', 'r'],
+            ['c', 'e', 'k', 'a', 'i', 'n'],
+            ['c', 'e']
+          ]
+
+    def generate_sub_rule(self):
+        rule = ""
+        for num in sample(self.numbers, randrange(1,8)):
+            rule += str(num)
+            number_of_letters = randrange(1, len(self.letters[num-1]))
+            for letter in sample(self.letters[num-1], number_of_letters):
+                rule += letter
+        return rule
+
+    def generate_rule(self):
+        rule = "b" + self.generate_sub_rule() +\
+               "/s" + self.generate_sub_rule()
+        return rule
+
+    def process(self, rule, iteration):
+        i = 0
+        result = False
+        output = []
+
+        process = Popen(
+                    ["./ntzfind/ntzfind", rule, "w6", self.period, "k1", "v", "l200"],
+                    shell=False,
+                    stdout=PIPE
+                    )
+        
+        while True:
+            output.append(process.stdout.readline().decode('utf-8'))
+            if 'Starting search' in str(output[i]):
+                start = i+2
+            elif 'CPU time' in str(output[i]):
+                start = i+1
+            elif 'Length' in str(output[i]):
+                end = i
+            elif '0 spaceships' in str(output[i]):
+                del output[:]
+                break
+            elif 'Spaceship found' in str(output[i]):
+                with open(
+                    #self.golly_file_path+"pattern"+str(iteration+1)+".lif",
+                    "pattern"+str(iteration+1)+".lif",
+                    'w'
+                    ) as file:
+
+                    file.write("#Life 1.05\n")
+                    file.write('#' + rule + '\n')
+                    file.write("#Period " + str(self.period) + '\n')
+                    for k in range(end - start):
+                        file.write(output[start+k].strip().replace('o','*') + '\n')
+                        print(output[start+k].strip())
+                    del output[:]
+                    print('')
+                    result = True
+                    break
+            i += 1
+        return result
+
+    def run(self):
+        successes = 0
+        while successes < self.iterations:
+            rule = self.generate_rule()
+            if self.process(rule, successes):
+                successes += 1
+
---
+++ automons/main.py
@@ -0,0 +1,13 @@
+import auto_ntzfind.auto_ntzfind as auto_ntzfind
+
+def main():
+    auto_ntzfind.Auto_NTZfind(10, 4).run()
+
+
+    # Spaceships are written to a pattern file in Golly
+    # A golly script is run to convert the Life 1.05 files to RLE
+    # rle2apgcode
+
+
+if __name__ == '__main__':
+    main()
